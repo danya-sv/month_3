eb28eda2b3fe3e84955441084fec87b1af252fa9
danya-sv <sarapulovdanila@gmail.com>
2024-10-31 23:19:02 +0600

    tg_bot

---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from dotenv import dotenv_values
+import random
+
+token = dotenv_values(".env")["BOT_TOKEN"]
+bot = Bot(token=token)
+dp = Dispatcher()
+names = ["Иван", "Мария", "Алексей", "Ольга", "Дмитрий"]
+user_ids = set()
+
+@dp.message(Command("start"))
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in user_ids:
+        user_ids.add(user_id)
+        user_count = len(user_ids)
+        name = message.from_user.first_name
+        await message.answer(f"Привет, {name}! Наш бот обслуживает уже {user_count} пользователей.")
+
+@dp.message(Command("myinfo"))
+async def myinfo_handler(message: types.Message):
+    your_id = message.from_user.id
+    f_name = message.from_user.first_name
+    use_name = message.from_user.username
+    await message.answer(f'Ваш id - {your_id}\n'
+                         f'Ваше имя - {f_name.title()} \n'
+                         f'Ваш username - {use_name}')
+
+@dp.message(Command('random'))
+async def random_handler(message: types.Message):
+    random_name = random.choice(names)
+    await message.answer(f'Рандомное имя из списка - {random_name}')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
