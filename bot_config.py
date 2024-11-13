d512d723e0709a9b1753809a7a699676dd69fef0
danya-sv <sarapulovdanila@gmail.com>
2024-11-13 18:41:27 +0600

    nothing

--- bot_config.py
+++ bot_config.py
@@ -11,31 +11,31 @@ recipes = [
     {
         "name": "Паста Карбонара",
         "recipe": "Варим спагетти, добавляем обжаренный бекон, яйца и сыр Пекорино.",
-        "image_path": "resipes/pasta.jpg"
+        "image_path": "resipes/pasta.jpg",
     },
     {
         "name": "Салат Цезарь",
         "recipe": "Смешиваем листья салата, курицу, сухарики, добавляем соус Цезарь и посыпаем пармезаном.",
-        "image_path": "resipes/salat.jpg"
+        "image_path": "resipes/salat.jpg",
     },
     {
         "name": "Борщ",
         "recipe": "Варим бульон, добавляем свёклу, капусту, картофель, морковь и томатную пасту. Подаём со сметаной.",
-        "image_path": "resipes/borsch.jpg"
+        "image_path": "resipes/borsch.jpg",
     },
     {
         "name": "Том Ям",
         "recipe": "Варим креветки в кокосовом молоке с пастой Том Ям, добавляем грибы, лемонграсс и лайм.",
-        "image_path": "resipes/tomyam.jpg"
+        "image_path": "resipes/tomyam.jpg",
     },
     {
         "name": "Омлет с овощами",
         "recipe": "Взбиваем яйца с солью и перцем, добавляем обжаренные овощи (помидоры, перец, шпинат), жарим до готовности.",
-        "image_path": "resipes/omlet.jpg"
+        "image_path": "resipes/omlet.jpg",
     },
     {
         "name": "Чизкейк",
         "recipe": "Смешиваем сливочный сыр с сахаром и яйцами, заливаем на основу из печенья, выпекаем в духовке.",
-        "image_path": "resipes/cheesecake.jpg"
-    }
+        "image_path": "resipes/cheesecake.jpg",
+    },
 ]
